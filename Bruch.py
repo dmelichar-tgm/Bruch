fddf7a2937336ee90b019fdbb38853ea9c43a7de
Daniel Melichar <danielmelichar0905@gmail.com>
2015-09-21 09:03:48 +0200

    Initial Commit

---
+++ Bruch.py
@@ -0,0 +1,12 @@
+"""
+    @version: 21. September 2015
+    @author: Daniel Melichar
+"""
+
+class Bruch(object):
+
+    def __init__(self):
+        """
+        To-Do
+        :return:
+        """
